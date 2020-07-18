7fbf0f6255b76622c672f772935be04e54fefa3f
honorezemagho <honorezemagho@gmail.com>
2020-07-18 19:35:05 +0100

    adding Json placeholder for api consuming

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
+import  axios  from 'axios';
 
 
 class App extends Component {
@@ -7,7 +8,10 @@ class App extends Component {
     posts: []
   };
 
-
+  async componentDidMount(){
+    const  {data: posts} = await axios.get('https://jsonplaceholder.typicode.com/posts')
+    this.setState({ posts})
+  }
 
   handleAdd = () => {
     console.log("Add");
